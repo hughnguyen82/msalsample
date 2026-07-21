4a53342f0871065658cf0d6b6c874f36fdd12112
hughn <hugh@localhost>
2026-07-21 17:51:48 +0800

    new build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: "https://hughnguyen82.github.io/msalsample/",
+  base: "/msalsample/",
 
   build : {
     rolldownOptions : {
